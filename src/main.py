381d63f38bea3ce8eef494121f2ecc57e6c4b0d6
abdallah197 <abdullahbashir077@gmail.com>
2024-08-09 17:37:53 +0100

    added train funciton

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader, random_split
 from transformers import AutoTokenizer, AutoModel
+from sklearn.metrics import accuracy_score
 
 @dataclass
 class TrainConfig:
@@ -15,6 +16,7 @@ class TrainConfig:
     max_lr = 3e-5
     epochs = 4
     device = 'cpu'
+    out_dir = 'output'
 
 config = TrainConfig()
 ### config ###
@@ -94,3 +96,54 @@ class AlbertModelForClassification(nn.Module):
 
 
 model = AlbertModelForClassification(model_id)
+
+def train(config: TrainConfig, model: AlbertModelForClassification, train_dataloader:DataLoader, test_dataloader: DataLoader):
+    optimizer = torch.optim.AdamW(model.parameters(), lr=config.max_lr)
+    scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, config.max_lr, epochs=config.epochs,
+                                                    steps_per_epoch=len(train_dataloader))
+
+    best_accuracy = 0.0
+    best_model = model
+
+    for epoch in range(config.epochs):
+        print(f'Epoch: {epoch}')
+        total_loss = 0.0
+
+        for step, item in enumerate(train_dataloader):
+            model.train()
+            item = {key: val.to(config.device) for key, val in item.items()}
+            optimizer.zero_grad()
+            logits, loss = model()
+            total_loss += loss.detach()
+            loss.backward()
+            optimizer.step()
+            scheduler.step()
+
+            if step % config.eval_steps:
+                print(f'train loss: {total_loss.detach().item() / config.eval_steps}')
+                model.eval()
+                preds, true_labels =[], []
+
+                for _, eval_item in enumerate(test_dataloader):
+                    eval_item = {key: val.to(config.device) for key, val in eval_item.items()}
+                    labels_ = eval_item['labels']
+                    with torch.no_grad():
+                        scores, _ = model(eval_item)
+                    preds_ = torch.argmax(scores, dim=1).tolist()
+                    preds.extend(preds_)
+                    true_labels.extend(labels_)
+                acc = accuracy_score(preds, true_labels)
+                print(f'step: {step}, acc: {acc}')
+                if acc > best_accuracy:
+                    best_accuracy = acc
+                    best_model = torch.save(model.state_dict(), config.out_dir)
+    return best_model
+
+
+
+
+
+
+
+
+
