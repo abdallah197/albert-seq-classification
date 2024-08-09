1a3f67f5f44dc40a6034c8940b5e5b911fe0144f
abdallah197 <abdullahbashir077@gmail.com>
2024-08-09 16:59:49 +0100

    added torch dataset and tested

--- src/main.py
+++ src/main.py
@@ -1,5 +1,36 @@
 import pandas as pd
-from torch.utils.data import DataLoader, Dataset
+import torch
+from torch.utils.data import Dataset
+from transformers import AutoTokenizer
 
-df = pd.read_csv('../data/imdb.csv')
+### config ###
+file_path = '../data/imdb.csv'
+model_id = 'albert/albert-base-v2'
 
+class TextClassification(Dataset):
+    def __init__(self, file_path, model_id):
+        dataframe = pd.read_csv(file_path)
+        self.tokenizer = AutoTokenizer.from_pretrained(model_id)
+        self.text = dataframe.review.tolist()
+        labels = dataframe.sentiment.tolist()
+        label2id = {'positive': 1, 'negative': 0}
+        self.labels = [label2id[label] for label in labels]
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, idx):
+        sentence = self.text[idx]
+        label = self.labels[idx]
+
+        tokenized_txt = self.tokenizer(sentence, truncation=True, max_length=self.tokenizer.model_max_length,
+                                       padding='max_length', return_tensors='pt')
+
+        # force a python dict type
+        tokenized_txt = {item: val.squeeze() for item, val in tokenized_txt.items()}
+        label = torch.tensor(label)
+        tokenized_txt['labels'] = label.type_as(tokenized_txt['input_ids'])
+        return tokenized_txt
+
+dataset = TextClassification(file_path, model_id)
+print(dataset[2])
